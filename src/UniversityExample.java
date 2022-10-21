3a4a8db0d1f39e62c814b818c8050409ce6947de
archil-nu <alelashvili@coe.neu.edu>
2022-10-21 16:52:04 -0400

    Removed code from main class. Assignment prep.

--- src/UniversityExample.java
+++ src/UniversityExample.java
@@ -28,25 +28,6 @@ public class UniversityExample {
         
         Department department = new Department("Information Systems");
         CourseCatalog coursecatalog = department.getCourseCatalog();
-        
-        Course course = coursecatalog.newCourse("app eng", "info 5100", 4);
-        System.out.println(course.getCourseNumber());
-        
-        CourseSchedule courseschedule = department.newCourseSchedule("Spring2022");
-
-        CourseOffer courseoffer = courseschedule.newCourseOffer("info 5100");
-        if (courseoffer==null)return;
-        courseoffer.generatSeats(10);
-        PersonDirectory pd = department.getPersonDirectory();
-        Person person = pd.newPerson("0112303");
-        StudentDirectory sd = department.getStudentDirectory();
-        StudentProfile student = sd.newStudentProfile(person);
-        CourseLoad courseload = student.newCourseLoad("Spring2022"); 
-        
-        courseload.newSeatAssignment(courseoffer); //register student in class
-        
-        int total = department.calculateRevenuesBySemester("Spring2022");
-        System.out.println("Total: " + total);
 
     }
 
